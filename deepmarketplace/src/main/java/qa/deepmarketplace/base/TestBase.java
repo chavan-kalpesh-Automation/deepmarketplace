c66252066d01629d0b4d8da144de48769918fa51
kalpeshsc <kalpeshsc@teamlocus.ent>
2024-03-21 19:42:57 +0530

    first commit

--- deepmarketplace/src/main/java/qa/deepmarketplace/base/TestBase.java
+++ deepmarketplace/src/main/java/qa/deepmarketplace/base/TestBase.java
@@ -1,7 +1,58 @@
 package qa.deepmarketplace.base;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class TestBase {
-public static void main(String []args) {
-	
-}
+	public static Properties prop;
+	public static WebDriver driver;
+
+	public TestBase() {
+		try {
+			prop = new Properties();
+			Path root = FileSystems.getDefault().getPath("").toAbsolutePath();
+			Path filePath = Paths.get(root.toString(), "src", "main", "java", "deepmarketplace", "confige",
+					"configue.properties");
+			FileInputStream fis = new FileInputStream(filePath.toString());
+			prop.load(fis);
+
+		} catch (FileNotFoundException e) {
+			e.getStackTrace();
+		} catch (IOException e) {
+			e.getStackTrace();
+
+		}
+	}
+
+	public static void initialisation() {
+		String browsername = prop.getProperty("browser");
+
+		String fireFoxDriverPath = prop.getProperty("firefoxdriverpath");
+
+		if (browsername.equals("chrome")) {
+			WebDriverManager.chromedriver().setup();
+
+			driver = new ChromeDriver();
+
+		} else if (browsername.equals("firefox")) {
+			System.setProperty("webdriver.gecko.driver", fireFoxDriverPath);
+			driver = new ChromeDriver();
+
+		}
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+
+		driver.get(prop.getProperty("url"));
+
+	}
 }
