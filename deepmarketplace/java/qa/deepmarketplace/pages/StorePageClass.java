5f7fd64ca00a75e1d8521a1024d53d6279d40993
kalpeshsc <kalpeshsc@teamlocus.ent>
2024-06-06 15:13:31 +0530

    WebElement Accessibility update

--- deepmarketplace/java/qa/deepmarketplace/pages/StorePageClass.java
+++ deepmarketplace/java/qa/deepmarketplace/pages/StorePageClass.java
@@ -1,7 +1,13 @@
 package qa.deepmarketplace.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 import qa.deepmarketplace.base.TestBase;
 
 public class StorePageClass extends TestBase{
 
+	
+	@FindBy (xpath="")
+	private WebElement locator;
 }
